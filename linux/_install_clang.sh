99f878a3289889be5e9be864fc6df8d25e2ccc1d
I <wx257osn2@yahoo.co.jp>
2014-09-09 02:29:25 +0900

    add clang 3.0/3.1. fix some mistake

--- linux/_install_clang.sh
+++ linux/_install_clang.sh
@@ -10,6 +10,21 @@ PackageVersion=`make_package_version $ProgramVersion`
 BuildWorkPath=`buildworkpath $Program`
 cd $BuildWorkPath
 
+if cd llvm; then
+    cd tools
+    if [ -e clang ]; then
+        rm -rf clang
+    fi
+    cd ..
+    cd projects
+    if [ -e compiler-rt ]; then
+        rm -rf compiler-rt
+    fi
+    cd ..
+    cd ..
+fi
+
+
 # 1
 if cd llvm; then
     git checkout master
@@ -20,7 +35,6 @@ else
 fi
 
 # 2
-cd llvm/tools
 if cd clang; then
     git checkout master
     git pull
@@ -32,11 +46,7 @@ fi
 cd clang
 GitClangVersion=`get_git_rev`
 cd ../
-
-cd ../..
-
 # 3
-cd llvm/tools/clang/tools
 if cd extra; then
     git checkout master
     git pull
@@ -44,10 +54,7 @@ if cd extra; then
 else
     git clone http://llvm.org/git/clang-tools-extra extra
 fi
-cd ../../../..
-
 # 4
-cd llvm/projects
 if cd compiler-rt; then
     git checkout master
     git pull
@@ -55,23 +62,58 @@ if cd compiler-rt; then
 else
     git clone http://llvm.org/git/compiler-rt
 fi
-cd ../..
 
 if [ "$ProgramVersion" == "head" ]; then
     RevedPackageVersion="$PackageVersion.$GitClangVersion"
+    cd extra
+    ExtraPath=`pwd`
+    cd ..
+    cd llvm/tools
+    cp -r ../../clang clang
+    cd ../..
+    cd llvm/tools/clang/tools
+    cp -r $ExtraPath extra
+    cd ../../../..
+    cd compiler-rt
+    CompilerRTPath=`pwd`
+    cd ..
+    cd llvm/projects
+    cp -r $CompilerRTPath compiler-rt
+    cd ../..
+    echo "Version => $RevedPackageVersion"
 else
-    RevedPackageVersion="$PackageVersion"
+    IntegerPackageVersion=${ProgramVersion:0:1}${ProgramVersion:2:1}
     cd llvm
-    git checkout release_${ProgramVersion:0:1}${ProgramVersion:2:1}
-    cd tools/clang
-    git checkout release_${ProgramVersion:0:1}${ProgramVersion:2:1}
-    cd tools/extra
-    git checkout release_${ProgramVersion:0:1}${ProgramVersion:2:1}
-    cd ../../../../projects/compiler-rt
-    git checkout release_${ProgramVersion:0:1}${ProgramVersion:2:1}
-    cd ../../..
+    git checkout release_$IntegerPackageVersion
+    cd ..
+    cd clang
+    git checkout release_$IntegerPackageVersion
+    cd ..
+    if [ $IntegerPackageVersion -gt 32 ]; then
+        cd extra
+        git checkout release_$IntegerPackageVersion
+        cd ..
+        cd compiler-rt
+        git checkout release_$IntegerPackageVersion
+        cd ..
+        cd extra
+        ExtraPath=`pwd`
+        cd ..
+        cd llvm/tools
+        cp -r ../../clang clang
+        cd ../..
+        cd llvm/tools/clang/tools
+        cp -r $ExtraPath extra
+        cd ../../../..
+        cd compiler-rt
+        CompilerRTPath=`pwd`
+        cd ..
+        cd llvm/projects
+        cp -r $CompilerRTPath compiler-rt
+        cd ../..
+    fi
+    echo "Version => $PackageVersion"
 fi
-echo "Version => $RevedPackageVersion"
 
 #
 IFS="?" read Cur Conf <<< "`make_build_dir clang-build`"
@@ -86,6 +128,9 @@ $Conf/llvm/configure \
     --host=$BinarySystem \
     --enable-optimized \
     --enable-assertions=no \
-    --enable-targets=host-only \
-    && make_edge_deb_from_dir clang $RevedPackageVersion $Cur $InstallPrefix
-
+    --enable-targets=host-only
+if [ "$ProgramVersion" == "head" ]; then
+    make_edge_deb_from_dir clang $RevedPackageVersion $Cur $InstallPrefix
+else
+    make_versioned_deb_from_dir clang $PackageVersion $Cur $InstallPrefix
+fi
